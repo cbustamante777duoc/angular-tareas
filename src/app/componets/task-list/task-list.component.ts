6ed9ca32cf71c8b2961f969bd42657690897598d
cristian <cra.bustamante@alumnos.duoc.cl>
2020-09-02 14:10:04 -0400

    agregando funciones addtask, showInputTextTask

--- src/app/componets/task-list/task-list.component.ts
+++ src/app/componets/task-list/task-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../../interfaces/Task';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.scss']
+  styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent implements OnInit {
 
@@ -11,14 +12,50 @@ export class TaskListComponent implements OnInit {
   public taskListNoCompleted:any = [];
   public taskListCompleted:any = [];
 
+  public showInputTask: boolean;
+  public errorInput:boolean;
+
+
 
   constructor() { 
     this.taskListCompleted = [];
     this.taskListNoCompleted = [];
+    this.showInputTask = false;
+    this.errorInput = false;
     
   }
 
   ngOnInit() {
+  }
+
+  //funcion que va a cambiar el valor de la variable showInputTask a true
+  showInputTextTask(){
+    this.showInputTask = true;
+  }
+
+  addTask(description){
+
+    if (description) {
+      const task: Task ={
+        'date': new Date(),
+        'description': description,
+        'completed': false
+      }
+  
+      //añadiendo una tarea al arreglo de no completadas
+      this.taskListNoCompleted.push(task);
+      this.errorInput = false;
+      this.showInputTask = false;
+
+      console.log(this.taskListNoCompleted);
+      
+    } else {
+      this.errorInput = true;
+      
+    }
+
+    
+
   }
 
 }
---
+++ src/app/interfaces/Task.ts
@@ -0,0 +1,6 @@
+export interface Task {
+    'date': Date,
+    'description': string,
+    'completed': boolean
+
+}
